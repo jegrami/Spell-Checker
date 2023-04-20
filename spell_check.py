0f3bdcc487b9c35a028e4ca0a854db4f6a16ad57
Jeremiah Igrami <jegrami.dev@gmail.com>
2023-04-20 11:05:53 +0100

    Intitial Commit

---
+++ spell_check.py
@@ -0,0 +1,20 @@
+
+from textblob import TextBlob 
+
+def check_spelling():
+    ''' Prompts user for a word and returns correct version if word is mispelled'''
+    while True:
+        word = input("Please enter a word:\n").lower()
+        correct_word = str(TextBlob(word).correct())
+        if correct_word != word:
+           response = input(f"Did you mean {correct_word}? (yes/no):\n ").lower()
+           if response == 'yes':
+               return correct_word.title()
+           
+           elif response == 'no':
+               print(f"Sure about '{word}'? The word is not recognized. Check and try again.")
+        else:
+            return word.title()
+        continue
+word = check_spelling()
+print(f"The word is {word.title()}.")
